b5ec21c7c70c8252212b9c5f866f2ade717a7ccd
Drew Franklin <drew@drewfranklin.com>
2008-08-16 14:18:28 -0700

    added banks.h

---
+++ banks.h
@@ -0,0 +1,53 @@
+// general midi drum notes
+#define record     38
+#define overdub    39
+#define mute       42
+#define multiply   40
+#define insert     41
+#define undo       43
+#define next       44
+#define replace    45
+#define substitute 46
+#define insertOnly 47
+#define speedy     48
+#define reverse    49
+
+int bank0A = record;
+int bank0B = overdub;
+int bank0C = mute;
+
+int bank1A = multiply;
+int bank1B = insert;
+int bank1C = undo;
+
+int bank2A = next;
+int bank2B = replace;
+int bank2C = substitute;
+
+int bank3A = insertOnly;
+int bank3B = speedy;
+int bank3C = reverse;
+
+int bank4A = record;
+int bank4B = overdub;
+int bank4C = mute;
+
+int bank5A = record;
+int bank5B = overdub;
+int bank5C = mute;
+
+int bank6A = record;
+int bank6B = overdub;
+int bank6C = mute;
+
+int bank7A = record;
+int bank7B = overdub;
+int bank7C = mute;
+
+int bank8A = record;
+int bank8B = overdub;
+int bank8C = mute;
+
+int bank9A = record;
+int bank9B = overdub;
+int bank9C = mute;
